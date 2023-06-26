c710170a01d10df77191ea2e5ac42e3094911223
Eddie_kim <apst@apstinc.com>
2023-06-26 16:48:39 +0900

    2023_0626_2

---
+++ src/app/products/upload/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+import Button from "@/components/Button";
+import Container from "@/components/Container";
+import Heading from "@/components/Heading";
+import Input from "@/components/Input";
+import React, { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+const ProductUploadPage = () => {
+  const [isLoadging, setIsLoadging] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      title: "",
+      description: "",
+      category: "",
+      latitude: 33.5563,
+      longitude: 126.79581,
+      imageSrc: "",
+      price: 1,
+    },
+  });
+
+  const onSubmit:SubmitHandler<FieldValues> = (data) => {
+
+  }
+  return (
+    <Container>
+      <div className=" max-w-screen-lg mx-auto">
+        <form className="flex flex-col gap-8" onSubmit={handleSubmit(onSubmit)}>
+        <Heading
+            title = "product Upload"
+            subtitle = "upload your product"
+        />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoadging}
+          register={register}
+          errors={errors}
+          required
+        />
+        
+        <hr />
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoadging}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id="price"
+          label="Price"
+          formatPrice
+          disabled={isLoadging}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+
+        <div
+          className="
+            grid
+            grid-cols-1
+            md:gird-cols-2
+            gap-3
+            max-h-[50vh]
+            overflow-y-auto
+            "
+        >
+          {/*category*/}
+        </div>
+        <hr />
+
+        {/*kakao map*/}
+
+        <Button label="상품 생성하기" />
+
+        </form>
+        
+      </div>
+    </Container>
+  );
+};
+
+export default ProductUploadPage;
