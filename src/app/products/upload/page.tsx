a984a2f806540caf207ab31a38d7a59b706a7740
Eddie_kim <apst@apstinc.com>
2023-06-27 17:32:16 +0900

    2023_06_27

--- src/app/products/upload/page.tsx
+++ src/app/products/upload/page.tsx
@@ -2,11 +2,19 @@
 import Button from "@/components/Button";
 import Container from "@/components/Container";
 import Heading from "@/components/Heading";
+import ImageUpload from "@/components/ImageUpload";
 import Input from "@/components/Input";
+import KakaoMap from "@/components/KakaoMap";
+import CategoryInput from "@/components/categories/CategoryInput";
+import { categories } from "@/components/categories/categories";
+import axios from "axios";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const ProductUploadPage = () => {
+  const router = useRouter()
   const [isLoadging, setIsLoadging] = useState(false);
   const {
     register,
@@ -27,8 +35,33 @@ const ProductUploadPage = () => {
     },
   });
 
-  const onSubmit:SubmitHandler<FieldValues> = (data) => {
+  const imageSrc = watch('imageSrc')
 
+  const category = watch('category')
+
+  const latitude = watch('latitude')
+
+  const longitude = watch('longitude')
+
+  const KakaoMap = dynamic(()=> import('../../../components/KakaoMap'),{
+    ssr:false
+  })
+
+  const onSubmit:SubmitHandler<FieldValues> = (data) => {
+    setIsLoadging(true);
+    axios.post('/api/products',data)
+      .then(response=>{
+        router.push(`/products/${response.data.id}`)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+      .finally(()=>{
+        setIsLoadging(false);
+      })
+  }
+  const setCustomValue = (id:string,value:any) => {
+    setValue(id,value)
   }
   return (
     <Container>
@@ -38,6 +71,7 @@ const ProductUploadPage = () => {
             title = "product Upload"
             subtitle = "upload your product"
         />
+        <ImageUpload onChange={(value)=>setCustomValue('imageSrc',value)} value={imageSrc} />
         <Input
           id="title"
           label="Title"
@@ -70,19 +104,35 @@ const ProductUploadPage = () => {
 
         <div
           className="
-            grid
-            grid-cols-1
-            md:gird-cols-2
-            gap-3
-            max-h-[50vh]
-            overflow-y-auto
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-3
+          max-h-[50vh]
+          overflow-y-auto
             "
         >
-          {/*category*/}
+        {categories.map((item)=>(
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category)=>setCustomValue('category',category)}
+              selected={category === item.path}
+              label={item.label}
+              icon={item.icon}
+              path={item.path}
+            />
+
+          </div>
+        ))}
         </div>
         <hr />
 
-        {/*kakao map*/}
+        <KakaoMap
+          setCustomValue={setCustomValue}
+          latitude={latitude}
+          longitude={longitude}
+
+        />
 
         <Button label="상품 생성하기" />
 
